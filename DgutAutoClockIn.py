434bb5f7330e4219c646a1870335a2241f74185f
Karle <970587844@qq.com>
2022-09-23 15:00:14 +0800

    9.23update-2

--- DgutAutoClockIn.py
+++ DgutAutoClockIn.py
@@ -21,26 +21,27 @@ try:
     browser.find_element(By.ID, "password").send_keys("--此处输入密码--\n")  # 输入密码并登录
     time.sleep(10)
 
-    browser.execute_script("document.querySelectorAll('.van-cell__title')[7].click()")  #点击选择身体状况选项
+    browser.execute_script("document.querySelectorAll('.van-cell__title')[7].click()")  # 点击选择身体状况选项
     time.sleep(1)
-    browser.execute_script("document.querySelectorAll('.van-picker-column__item')[0].click()")  #选择身体状况良好
+    browser.execute_script("document.querySelector('.van-picker__confirm').click()")  # 提交身体状况
     time.sleep(1)
-    browser.execute_script("document.querySelector('.van-picker__confirm').click()")    #提交身体状况
+
+    element_body_temperature = browser.find_element(By.TAG_NAME, 'textarea')
+    element_body_temperature.send_keys(Keys.CONTROL, 'a')  # 清空体温
+    element_body_temperature.send_keys('36.5')  # 输入体温
     time.sleep(1)
 
     browser.execute_script("document.querySelectorAll('.van-cell__title')[10].click()")  # 点击选择是否在校
     time.sleep(1)
-    browser.execute_script("document.querySelectorAll('.van-picker-column__item')[0].click()")  # 选择在ssl校区
-    time.sleep(1)
     browser.execute_script("document.querySelectorAll('.van-picker__confirm')[1].click()")  # 提交在校情况
     time.sleep(1)
 
-    browser.find_element(By.TAG_NAME, 'textarea').send_keys("36.5")  # 输入体温
+    browser.execute_script("document.querySelectorAll('.van-cell__title')[36].click()")  # 选择最后一次核酸日期
     time.sleep(1)
-    browser.execute_script("document.querySelector('.van-picker__confirm').click()")  # 提交体温
+    browser.execute_script("document.querySelectorAll('.van-picker__confirm')[2].click()")  # 提交核酸日期
     time.sleep(1)
 
-    #判断打卡结果
+    # 提交打卡并且判断打卡结果
     js_code = "let a = document.createElement('p');" \
               "let b = document.createTextNode('已经打过卡了');" \
               "let c = document.createTextNode('打卡成功');" \
@@ -50,7 +51,9 @@ try:
               "else if(content === '撤回重填') a.appendChild(b);" \
               "let timer = setInterval(function () {let content1 = document.querySelector('button').querySelector('span').innerHTML;if (content1 === '提交') a.appendChild(d);else if (content1 === '撤回重填') a.appendChild(b);document.body.appendChild(a);clearInterval(timer);}, 20000);"
     browser.execute_script(js_code)
-    time.sleep(5)
+    time.sleep(30)
+
+    print(browser.find_element(By.XPATH, "//p").text)
     if browser.find_element(By.XPATH, "//p").text == "已经打过卡了":
         result = "已经打过卡"
     elif browser.find_element(By.XPATH, "//p").text == "打卡成功已经打过卡了":
